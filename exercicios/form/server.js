ff6cfc31d0cd9303ef370567db62eda0e94d2b56
Diego <65690176+dmoprates@users.noreply.github.com>
2021-02-15 07:02:04 -0300

    Arquivos do Curso
    Commit dos arquivos da aula

---
+++ exercicios/form/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extender: true}))
+app.post('/usuarios', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1>Parabéns</h1>')
+})
+
+app.listen(3003)
